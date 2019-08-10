fcfa2574a6ee0ba60a267939ac7a4dde0b672f17
EricDong <eric.dong@outlook.com>
2019-08-10 18:18:35 +1000

    add new addons

---
+++ AddOns/Bagnon/external/Wildpants/components/inventory.lua
@@ -0,0 +1,33 @@
+--[[
+	inventory.lua
+		A specialized version of the standard frame for the inventory
+--]]
+
+local ADDON, Addon = ...
+local Frame = Addon:NewClass('InventoryFrame', 'Frame', Addon.Frame)
+Frame.Title = LibStub('AceLocale-3.0'):GetLocale(ADDON).TitleBags
+Frame.Bags = {BACKPACK_CONTAINER, 1, 2, 3, 4}
+Frame.MainMenuButtons = {
+	MainMenuBarBackpackButton,
+	CharacterBag0Slot, CharacterBag1Slot, CharacterBag2Slot, CharacterBag3Slot
+}
+
+function Frame:OnShow()
+	Addon.Frame.OnShow(self)
+	self:HighlightMainMenu(true)
+end
+
+function Frame:OnHide()
+	Addon.Frame.OnHide(self)
+	self:HighlightMainMenu(false)
+end
+
+function Frame:HighlightMainMenu(checked)
+	-- for i, button in pairs(self.MainMenuButtons) do
+	-- 	if checked then
+	-- 		button.SlotHighlightTexture:Show()
+	-- 	else
+	-- 		button.SlotHighlightTexture:Hide()
+	-- 	end
+	-- end
+end
