a3c2365da0cef3f46d4e9e4e804810e879f4f2d6
EricDong <eric.dong@outlook.com>
2019-08-27 16:48:06 +1000

    Add files via upload

---
+++ 按我修改字体.bat
@@ -0,0 +1,20 @@
+﻿@echo off
+@echo FONT
+Copy 1.ttf ARIALN.TTF
+copy 1.ttf FRIZQT__.TTF
+copy 1.ttf bHEI00M.TTF
+copy 1.ttf bHEI01B.TTF
+copy 1.ttf bKAI00M.TTF
+copy 1.ttf bLEI00D.TTF
+copy 1.ttf MORPHEUS.TTF
+copy 1.ttf SKURRI.TTF
+copy 1.ttf FRIENDS.TTF
+copy 1.ttf ARHei.TTF
+copy 1.ttf ARKai_C.TTF
+copy 1.ttf ARKai_T.TTF
+copy 1.ttf 2002.ttf
+copy 1.ttf 2002B.ttf
+copy 1.ttf K_Damage.TTF
+copy 1.ttf K_Pagetext.TTF
+@echo FINISH!
+pause
